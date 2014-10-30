17a49dae86fc60d552c89548ad4771dd2b4c78e9
Pinkii- <pinkiibiza@gmail.com>
2014-10-30 12:56:23 +0100

    FuncionsOrdreSuperior1 donde

---
+++ funcionsOrdreSuperior1.hs
@@ -0,0 +1,22 @@
+eql :: [Int] -> [Int] -> Bool
+eql l1 l2 = l1 == l2
+--eql [] [] = True
+--eql l1 [] = False
+--eql [] l2 = False
+--eql l1@(x:xs) l2@(y:ys)
+--    | x == y = eql xs ys
+--    | otherwise = False
+
+prod :: [Int] -> Int
+prod [] = 1
+prod xs = foldl (\a x -> a*x) 1 xs
+
+prodOfEvens :: [Int] -> Int
+prodOfEvens xs = prod (filter even xs)
+
+powersOf2 :: [Int]
+powersOf2 = iterate (*2) 1
+
+scalarProduct :: [Float] -> [Float] -> Float
+scalarProduct [] [] = 0
+scalarProduct xs ys = sum (zipWith (\x y -> x*y) xs ys)
