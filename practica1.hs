9a2e1b3348c31004ca1586a7156f11e7230a0a9f
Pinkii- <pinkiibiza@gmail.com>
2014-11-21 17:48:32 +0100

    shiet pura y dura

--- practica1.hs
+++ practica1.hs
@@ -35,32 +35,52 @@ validRewriteSystem s ws = and $ map (validRule s) ws
 type Estrategia a = [(Position,a)] -> [(Position,a)]
 
 oneStepRewrite :: Rewrite a => RewriteSystem a -> a -> Estrategia a -> a
-oneStepRewrite rules obj est  = obj -- No se que pide 
+oneStepRewrite rules obj est = applyObjSubstitution obj $ matchObjRewriteS rules obj
+    where
+        matchObjRewriteS :: RewriteSystem a -> a -> [(Position,Substitution a)]
+        matchObjRewriteS rules obj = concat $ map (\(Rule a _) -> match obj a) rules
+        applyObjSubstitution :: a -> [(Position, Substitution a)] -> [a]
+        applyObjSubstitution obj l = map (apply obj) [ snd s | s <- l]
+--        filtraPorStrat :: Estrategia a -> [a] -> [a]
+
+--buscar de la paerte izquierda de la regla, el match con el objeto y devuelve possubstitucion. Filtrar por la estrategia, que se le pasa la pos, y obj original.
 
 auxrewrite :: Rewrite a => RewriteSystem a -> a -> Estrategia a -> [a]
 auxrewrite rules obj est = iterate (\x -> evaluate(oneStepRewrite rules x est)) obj
 
---rewrite :: Rewrite a => RewriteSystem a -> a -> Estrategia a -> a
---rewrite rules obj est = take 1 $ dropWhile ((/=) (drop 1 $ todos)) $ todos
---    where todos = auxrewrite rules obj est
+rewrite :: (Rewrite a, Eq a) => RewriteSystem a -> a -> Estrategia a -> a
+rewrite rules obj est = fst $ head $ dropWhile (\x -> fst x /= snd x) $ zip todos (drop 1 $ todos)
+    where todos = auxrewrite rules obj est
 
 nrewrite :: Rewrite a => RewriteSystem a -> a -> Estrategia a -> Int -> a
 nrewrite rules obj est n = last $ take n $ auxrewrite rules obj est
 
+
 data RString = RString String
 
-instance Rewrite RString where
-    getVars s = []
+--instance Rewrite RString where
+--    getVars s = []
     
-    valid [] _ = True
-    valid sig@((s,i):xs) (RString st) = and (map (\x -> False) sig)-- No se que cojones es una signatura y como se spune que voy a saber si algo es valid
+--    valid [] _ = True
+--    valid sig@((s,i):xs) (RString st) = and (map (\x -> ) sig)-- No se que cojones es una signatura y como se spune que voy a saber si algo es valid
 
 --    match (RString s1) (RString s2) = sMatch s1 s2
 --        where 
 --            sMatch :: String -> String -> [(Position,Substitution a)]
 --            sMatch (c1:s1) (c2:s2) = 
     
-    evaluate s = s
+--    evaluate s = s
+
+readRString :: String -> RString
+readRString s = RString s
+
+readRStringSystem :: [(String,String)] -> RewriteSystem RString
+readRStringSystem l = map (\x -> (Rule (readRString (fst x)) (readRString (snd x)))) l
+
+instance Show (RString) where
+    show(RString s)= tail $ init $ (show s)
+
+data RTerm a = RTerm a [Rterm a]
 
 --las de mas abajo :> todas las que su camino no es prefijo de otro
 
