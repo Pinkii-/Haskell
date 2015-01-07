9fe719716bcd366af01ff8f7a46016813b658a58
Pinkii <pinkiibiza@gmail.com>
2015-01-08 00:59:47 +0100

    la web se rompe nomas

--- Python/Practica/main.py
+++ Python/Practica/main.py
@@ -4,6 +4,9 @@ import sys
 import urllib2
 import xml.etree.ElementTree as ET
 import unicodedata
+import csv
+
+"""utils"""
 
 class UrlProcessor(object):
 	def __init__(self, url):
@@ -11,6 +14,26 @@ class UrlProcessor(object):
 		f = urllib2.urlopen(self.url)
 		self.xml = f.read()
 		f.close()
+
+class CsvProcessor(object):
+	def __init__(self, myCsv):
+		self.csv = myCsv
+		self.content = csv.DictReader(open(self.csv), delimiter = ';')
+		for row in self.content:
+			self.header = []
+			for aux in row:
+				self.header.append(aux)
+			break		
+
+def removeAccents(s):
+	uS = unicode(s)
+	return unicodedata.normalize('NFKD', uS).encode('ascii', 'ignore')
+
+def mNormalize(s):
+	s = removeAccents(s)
+	return s.lower().strip()
+
+"""Events"""
 
 class Event(object):
 	def printEvent(self):
@@ -50,18 +73,10 @@ def getEvents():
 		events.append(e)
 	return events
 
-def removeAccents(s):
-	uS = unicode(s)
-	return unicodedata.normalize('NFKD', uS).encode('ascii', 'ignore')
-
-def mNormalize(s):
-	s = removeAccents(s)
-	return s.lower().strip()
-
 def getGoodEvents():
 	events = getEvents()
 	entrada = sys.argv[1]
-	entrada = entrada.lower().strip()
+	entrada = mNormalize(entrada)
 	restriccions = entrada.split(",")
 	goodEvents = []
 	import unicodedata
@@ -82,10 +97,117 @@ def getGoodEvents():
 			goodEvents.append(event)
 	return goodEvents
 
+"""Meteosat"""
+
+def getMeteo():
+	url = 'http://static-m.meteo.cat/content/opendata/ctermini_comarcal.xml'
+	xmlGetter = UrlProcessor(url)
+	root = ET.fromstring(xmlGetter.xml)
+	weather = []
+	for comarca in root.iter('comarca'):
+		capital = comarca.get('nomCAPITALCO')
+		if capital == 'Barcelona':
+			comarcaId = comarca.get('id')
+			# print 'Barcelona encontrada con la id', comarcaId
+	for prediccion in root.iter('prediccio'):
+		idPrediccio = prediccion.get('idcomarca')
+		if idPrediccio == comarcaId:
+			# print 'prediccion encontrada'
+			fPredic = {}
+			for dia in prediccion:
+				data = dia.get('data').replace('-','/')
+				weather.append((data,dia.get('probcalamati'),dia.get('probcalatarda')))
+	return weather
+
+"""Bus"""
+
+class Bus(object):
+	def printBus(self):
+		print "Bus", self.EQUIPAMENT
+
+def loadBus():
+	csv = 'Documents/ESTACIONS_BUS.csv'
+	buses = CsvProcessor(csv)
+	first = True
+	paradaBuses = []
+	for row in buses.content:
+		if first:
+			first = False
+		else:
+			b = Bus()
+			for header in buses.header:
+				b.__setattr__(header,row[header])
+			paradaBuses.append(b)
+	return paradaBuses
+
+"""Metro"""
+
+class Metro(object):
+	def printMetro(self):
+		print "Metro", self.EQUIPAMENT
+
+def loadMetro():
+	csv = 'Documents/TRANSPORTS.csv'
+	metro = CsvProcessor(csv)
+	first = True
+	paradaMetro = []
+	for row in metro.content:
+		if first:
+			first = False
+		else:
+			m = Metro()
+			for header in metro.header:
+				m.__setattr__(header,row[header])
+			paradaMetro.append(m)
+	return paradaMetro
+
+"""Bicing"""
+
+class Bicing(object):
+	def printBicing(self):
+		print 'Bicing', self.lat, self.long
+
+def loadBicing():
+	url = 'http://wservice.viabicing.cat/v1/getstations.php?v=1'
+	xmlGetter = UrlProcessor(url)
+	root = ET.fromstring(xmlGetter.xml)
+	bicing = []
+	for station in root.iter('station'):
+		b = Bicing()
+		for bic in station:
+			if (bic.tag == 'lat' or bic.tag == 'long' or bic.tag == 'status' or 
+				bic.tag == 'slots' or bic.tag == 'bikes' or bic.tag == 'street'):
+				b.__setattr__(bic.tag,bic.text)
+		bicing.append(b)
+	return bicing
+
+"""Web"""
+
+def drawWeb(events, weathers, buses, metros, bicings):
+	with open('index.html','w') as html:
+		html.write('<html><body><table>')
+		for event in events:
+			html.write('<tr><td><b>'+event.nom+'</b><br> ('+
+				event.carrer+' '+event.numero+' '+event.districte+' '+event.codi_postal+' '+event.municipi)
+
+
+			dayAndHour = event.data.split(' ')
+			someWeather = None
+			for weather in weathers:
+				if dayAndHour[0] == weather[0]:
+					someWeather = weather
+			print someWeather, dayAndHour[0], event.nom
+
+
+"""Main"""
+
 def main():
 	eventosElegidos = getGoodEvents()
-	for e in eventosElegidos:
-		e.printEvent()
+	isRainingMen = getMeteo()
+	bus = loadBus()
+	metro = loadMetro()
+	bicing =  loadBicing()
+	drawWeb(eventosElegidos, isRainingMen,bus,metro,bicing)
 
 
 if __name__ == '__main__':
